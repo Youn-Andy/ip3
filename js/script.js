dd4eb6f10bb157499c927cf6df503e9468425a35
andrew <ayietaphilip00@outlook.com>
2020-07-26 19:57:08 +0300

    Add an alert function that informs the user when a message is sent

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+function myFunction() {
+    alert("** John Doe we have received your message. Thank you for reaching out to us. **");
+}
+
 $(document).ready(function () {
     $("#design-ls").click(function () {
         $("#design").toggle();
